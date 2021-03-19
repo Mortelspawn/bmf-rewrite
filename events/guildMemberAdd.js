9f55cd290e5b96cf44610d34765cccf49d28e2a7
Ulrich Parmentier <ulrichp.official@gmail.com>
2021-03-19 15:21:56 +0100

    Modified welcome DM

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -12,17 +12,18 @@ module.exports = {
 
                 await dm.send({
                     embed: new MessageEmbed()
-                        .setTitle(`Welcome ${member.displayName} !\n\u200B`)
+                        .setTitle(`Welcome ${msg.author.username} !\n\u200B`)
                         .setDescription(' - In order to get verified and get access the the rest of the server, please choose one of the following options: \n\u200B')
-                        .addField(`
-                            1. Link with your osu! profile\n\u200B
-                             - You can link your osu! profile with:\n\`${prefix}link [profile-link]\`\n
-                            i.e: \`${prefix}link https://osu.ppy.sh/users/5331420\`
-                        `)
-                        .addField(`
-                            2. Not from osu! ?\n\u200B
-                            - If you are not an osu! player then do:\n\`${prefix}outsider\`\n
-                        `)
+                        .addField(
+                            "1. Link with your osu! profile\n\u200B",
+                            ` - You can link your osu! profile with: \`${prefix}link [profile-link]\`\n` +
+                            `i.e: \`${prefix}link https://osu.ppy.sh/users/5331420\``
+                        )
+                        .addField(
+                            "2. Not from osu! ?\n\u200B",
+                            `- If you are not an osu! player then do: \`${prefix}outsider\`\n`
+                        )
+                        .setColor('GOLD')
                 })
 
                 defaultCh.send(

e5ea5ae2324da232498bc48d0279c4fb12ba2879
Ulrich Parmentier <ulrichp.official@gmail.com>
2021-03-24 22:27:32 +0100

    Added last_update info

--- commands/util/update.js
+++ commands/util/update.js
@@ -1,6 +1,11 @@
 const { Client, Message, GuildMember, MessageEmbed } = require("discord.js");
 const core = require('./../../core/main.js')
 
+function msToHMS(ms) {
+    let d = new Date(ms)
+    return `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+}
+
 module.exports = {
     name: 'update',
     description: 'Updates your osu! data and gives your new ranks accordingly',
@@ -16,7 +21,7 @@ module.exports = {
         let user = await core.checkForUser(client, msg.author.id)
 
         if (user == null) return msg.channel.send('You have to be linked first to use "update", ' + `do \`${process.env.PREFIX}link [profile]\``)
-        if (!core.cooldown(user.data.player.last_update, 86400000)) return msg.channel.send('You can only use update every 24h')
+        if (!core.cooldown(user.data.player.last_update, 86400000)) return msg.channel.send('You can only use update every 24h, last update was at ' + msToHMS(user.data.player.last_update))
 
         let userData = await core.getOsuProfile(msg, user.data.player.user_id, user.data.player.mode)
         if (!userData) return

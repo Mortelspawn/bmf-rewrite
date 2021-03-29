ec1222ece6e39ac9085a9db792fd44f6d194acd7
Ulrich Parmentier <ulrichp.official@gmail.com>
2021-03-29 23:43:47 +0200

    Changes

--- commands/dev/query.js
+++ commands/dev/query.js
@@ -1,31 +1,44 @@
+const { Client, Message } = require("discord.js")
+
 module.exports = {
     name: 'query',
     dev: true,
 
+    /**
+     * Sends a query to the db
+     * @param {Client} client 
+     * @param {Message} msg 
+     * @param {Array} args 
+     */
     execute: async function(client, msg, args) {
-        // let query = args.join(" ").trim()
 
-        // let query = `UPDATE users SET discord -> a[0] = 'test' WHERE discord -> a ->> test = test`
+        switch (args[0]) {
 
-        // let data = testData
+            case 'select':
+                let select = await client.sql`SELECT * FROM users WHERE discord_id = ${msg.author.id}`
+                console.log(select[0].data)
+                break;
+            
+            case 'insert':
+                let user = await client.sql`SELECT * FROM users WHERE discord_id = ${msg.author.id}`
 
-        // let query = {
-        //     text: 'INSERT INTO users(discord_id, data) VALUES($1, $2)',
-        //     values: [msg.author.id, testData]
-        // }
+                user[0].data.player.last_update = 1616529600000
 
-        // testData.discord.username = "This_is_a_test"
+                let values = {
+                    data: JSON.stringify(user[0].data, null, 0)
+                }
 
-        // let query = {
-        //     text: `UPDATE users SET data = $1 WHERE discord_id = ${msg.author.id}`,
-        //     values: [testData]
-        // }
+                await client.sql`
+                    UPDATE users 
+                    SET ${client.sql(values, 'data')}
+                    WHERE discord_id = ${msg.author.id}
+                `
+                break;
+            
+            default:
+                let test = await client.sql`DELETE FROM users WHERE discord_id = ${msg.author.id}` 
+                console.log(test)
 
-        // let query = {
-        //     text: `SELECT * FROM users WHERE discord_id = ${msg.author.id}`
-        // }
-
-        let test = await client.sql`DELETE FROM users WHERE discord_id = 717016159945228288` 
-        console.log(test)
+        }
     }
 }
